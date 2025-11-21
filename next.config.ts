a24cca7d5b5b1bc4610d7ace1ad28db395eb56ad
hubert hu <hubertsilvasousa10@gmail.com>
2025-11-21 14:33:47 +0000

    import type { NextConfig } from 'next';
    const nextConfig: NextConfig =

--- next.config.ts
+++ next.config.ts
@@ -3,36 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
