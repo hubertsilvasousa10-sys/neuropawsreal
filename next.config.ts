33401c136a3c49ef9195b9729e4e5eabdc2020d4
hubert hu <hubertsilvasousa10@gmail.com>
2025-11-22 05:30:46 +0000

    deploy netlify normal

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["images.unsplash.com", "via.placeholder.com"], // coloque aqui os domínios das imagens externas que você usa
+  },
+};
 
 export default nextConfig;
+
